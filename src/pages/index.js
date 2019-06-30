b365d396f7d3861bef4a43de4ca66fdfae8b4261
Ross Dyson <dyson.ross@gmail.com>
2019-06-30 15:52:57 +0900

    add antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Button } from 'antd';
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
     <h1>Welcome to React-Static</h1>
+    <Button icon="search" type="danger" onClick={() => console.log('yes')}>What</Button>
   </div>
 )
