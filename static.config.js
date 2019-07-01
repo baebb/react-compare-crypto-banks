46bd65a8289be788d0949c5dc55a7e2c1648737e
Ross Dyson <dyson.ross@gmail.com>
2019-07-01 17:37:23 +0900

    add meta tags

--- static.config.js
+++ static.config.js
@@ -1,36 +1,57 @@
-import path from 'path'
-import axios from 'axios'
+import React from 'react';
+import path from 'path';
+import axios from 'axios';
 
 export default {
-  getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+    Document: ({
+                   Html,
+                   Head,
+                   Body,
+                   children,
+                   state: { siteData, renderMeta },
+               }) => (
+        <Html lang="en-US">
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta property="og:title" content="Compare Crypto Banks" />
+                <meta property="og:url" content="https://www.comparecryptobanks.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:description" content="Easily compare crypto finance: Compare all crypto saving accounts and crypto loans" />
+                <meta property="og:image" content="http://some-react-static-website/img/OG_thumb.jpg" />
+            </Head>
+            <Body>{children}</Body>
+        </Html>
+    ),
+    getRoutes: async () => {
+        const { data: posts } = await axios.get(
+            'https://jsonplaceholder.typicode.com/posts'
+        );
 
-    return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
-  plugins: [
-    [
-      require.resolve('react-static-plugin-source-filesystem'),
-      {
-        location: path.resolve('./src/pages'),
-      },
+        return [
+            {
+                path: '/blog',
+                getData: () => ({
+                    posts,
+                }),
+                children: posts.map(post => ({
+                    path: `/post/${post.id}`,
+                    template: 'src/containers/Post',
+                    getData: () => ({
+                        post,
+                    }),
+                })),
+            },
+        ];
+    },
+    plugins: [
+        [
+            require.resolve('react-static-plugin-source-filesystem'),
+            {
+                location: path.resolve('./src/pages'),
+            },
+        ],
+        require.resolve('react-static-plugin-reach-router'),
+        require.resolve('react-static-plugin-sitemap'),
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
-  ],
-}
+};
