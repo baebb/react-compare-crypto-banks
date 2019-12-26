b65e6991887c23f87522ccf72c5895466dcee104
Ross <me@rossdyson.com>
2019-12-26 20:03:14 +0900

    yea nice

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 // UI Dependencies
-import { Button, Pane, Text, Heading } from 'evergreen-ui';
+import { Pane, Text, Heading } from 'evergreen-ui';
 
 // Data
 import items from '../demo';
@@ -11,12 +11,19 @@ import BlockFiLogo from '../blockfi.png';
 export default () => (
     <div className="content">
         <div className="banner">
+            <h1>Test</h1>
+            <p>Earn cash rewards</p>
+            <a href="http://rossdyson.com" target="_blank">
+                <button  type="button" className="callout-button callout-button--primary">
+                    Apply now
+                </button>
+            </a>
             <Heading
                 size={600}
                 marginTop={0}
                 is="h4"
             >
-                Crypto Banks is ready
+                Segemnt is trash
             </Heading>
         </div>
         <div className="items">
@@ -30,22 +37,22 @@ export default () => (
                     <div className="row">
                         <div className="col-xs-12 col-sm-3">
                             <div
-                                style={{ textAlign: 'center', marginBottom: 16, minHeight: 140}}
+                                style={{ textAlign: 'center', marginBottom: 16, minHeight: 100 }}
                                 className="img-center-frame"
                             >
                                 <img src={BlockFiLogo} style={{ maxWidth: 240 }} />
                             </div>
                             <div style={{ textAlign: 'center', marginBottom: 8 }}>
-                                <a target="_blank" href={url} rel="noopener noreferrer">
-                                    <Button
-                                        appearance="primary"
-                                        intent="success"
-                                        height={48}
-                                        iconBefore="tick"
-                                    >
-                                        Apply Now
-                                    </Button>
-                                </a>
+                                {/*<a target="_blank" href={url} rel="noopener noreferrer">*/}
+                                {/*    <Button*/}
+                                {/*        appearance="primary"*/}
+                                {/*        intent="success"*/}
+                                {/*        height={48}*/}
+                                {/*        iconBefore="tick"*/}
+                                {/*    >*/}
+                                {/*        Apply Now*/}
+                                {/*    </Button>*/}
+                                {/*</a>*/}
                             </div>
                             <div style={{ textAlign: 'center' }}>
                                 <Text size={400} color="muted">on {companyName}'s website</Text>
