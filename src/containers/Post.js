8a2efe7f0743476ba51f42b16f7fdb227662b960
Ross <me@rossdyson.com>
2020-01-29 16:39:08 +1100

    markdown support

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,17 +1,20 @@
+// NPM Dependencies
 import React from 'react';
 import { useRouteData } from 'react-static';
+import ReactMarkdown from 'react-markdown';
 //
 import { Link } from 'components/Router';
 
 export default function Post() {
     const { post } = useRouteData();
+    const { title, body } = post;
 
     return (
         <div>
             <Link to="/blog/">{'<'} Back</Link>
             <br />
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
+            <h3>{title}</h3>
+            <ReactMarkdown source={body} />
         </div>
     );
 }
