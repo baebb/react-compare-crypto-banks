e9596f40c0691f5e6dbfc70bef2836709b1069ea
Ross <me@rossdyson.com>
2020-02-09 18:23:04 +1100

    add favicon

--- static.config.js
+++ static.config.js
@@ -5,7 +5,7 @@ import React from 'react';
 // import axios from 'axios';
 const contentful = require('contentful');
 
-const buildEnv = process.env.BUILD_ENV || 'prod';
+const buildEnv = process.env.BUILD_ENV || 'staging';
 
 const config = {
     prod: {
@@ -39,7 +39,15 @@ export default {
                 <meta property="og:url" content="https://www.comparecryptobanks.com" />
                 <meta property="og:type" content="website" />
                 <meta property="og:description" content="Easily compare crypto finance: Compare all crypto saving accounts and crypto loans" />
-                <meta property="og:image" content="http://some-react-static-website/img/OG_thumb.jpg" />
+                {/*<meta property="og:image" content="http://some-react-static-website/img/OG_thumb.jpg" />*/}
+
+                <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png" />
+                <link rel="manifest" href="./favicon/site.webmanifest" />
+                <link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#00a300" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <Body>{children}</Body>
         </Html>
