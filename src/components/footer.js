a4d59ca842c6c95548a239efe5577b7088ffd156
Ross <me@rossdyson.com>
2020-02-27 15:38:33 +0900

    footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,48 @@ import IconLogo from '../../public/images/logo/icon01.svg';
 export default function Footer() {
     return (
         <footer className="footer">
-            <div className="footer__content">
-                lol
+            <div className="footer-content">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6">
+                        <div className="footer-content__links">
+                            <div>
+                                <Link to="/reviews" className="text-link text-link--hover-green">
+                                    Reviews
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/blog" className="text-link text-link--hover-green">
+                                    Blog
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/about" className="text-link text-link--hover-green">
+                                    About
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                        <p className="footer-content__disclaimer-text">
+                            Disclaimer: Compare Crypto Banks strives to keep its information accurate and up to date. This information may be different than what you see when you visit a financial institution, service provider or specific product’s site. All financial products, shopping products and services are presented without warranty. When evaluating offers, please review the financial institution’s Terms and Conditions.
+                        </p>
+                    </div>
+                </div>
+                <div className="footer-content__brand-container">
+                    <div className="footer-content__brand-img">
+                        <Link to="/">
+                            <img src={IconLogo} />
+                        </Link>
+                    </div>
+                    <div className="footer-content__brand-text">
+                        <div>© 2020 Baebb OU, trading as Compare Crypto Banks</div>
+                        <div className="footer-content__brand-legal-links">
+                            <Link to="/privacy" className="text-link text-link--hover-green">
+                                Privacy Policy
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     );
