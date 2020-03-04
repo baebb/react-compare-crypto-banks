2ef75bff34a60df932d92ebff472a7b53c783aea
Ross <me@rossdyson.com>
2020-03-04 13:25:41 +0900

    try this package 3

--- static.config.js
+++ static.config.js
@@ -108,10 +108,10 @@ export default {
         ];
     },
     plugins: [
-        [
-            'react-static-plugin-favicons',
-            { inputFile: path.resolve('./public/images/logo/ccb-temp-icon.svg') },
-        ],
+        // [
+        //     'react-static-plugin-favicons',
+        //     { inputFile: path.resolve('./public/images/logo/ccb-temp-icon.svg') },
+        // ],
         [
             require.resolve('react-static-plugin-google-tag-manager'),
             { id: config[buildEnv].GOOGLE_TAG_MANAGER_ID }
