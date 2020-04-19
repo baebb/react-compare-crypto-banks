15445ba464d21913917bc92ab2c8facb2af6f009
Ross <me@rossdyson.com>
2020-04-19 21:33:33 +0900

    handle non-v2 content

--- src/containers/review-page.js
+++ src/containers/review-page.js
@@ -46,6 +46,74 @@ export default function ReviewPage() {
 
     const metaTitle = `${companyName} Review | DeFi Nerd`;
 
+    if (!companyReview) {
+        return (
+            <div className="review-page">
+                <Head>
+                    <title>{metaTitle}</title>
+                    <meta name="description" content={metaDescription} />
+                    <meta property="og:title" content={metaTitle} />
+                    <meta property="og:description" content={metaDescription} />
+                </Head>
+                <div className="header-section">
+                    <h5 className="header-section__editorial-text">Editorial Review</h5>
+                    <h1 className="header-section__title-text">{reviewTitle}</h1>
+                    <div className="header-section__description-text">
+                        <ReactMarkdown source={description} />
+                    </div>
+                    <div className="header-section__review-details">
+                        <span className="header-section__author-name">
+                            {authorName}
+                        </span>
+                        <span className="header-section__publish-time">
+                            {cleanPublishDate}
+                        </span>
+                    </div>
+                </div>
+                <div className="review-summary-section">
+                    <h2>Our Take</h2>
+                    <div className="review-summary-section__rating-wrapper">
+                        <div className="review-summary-section__rating-number">
+                            {rating.toFixed(1)}
+                        </div>
+                        <div>
+                            <div className="review-summary-section__rating-text">
+                                Editor's rating
+                            </div>
+                            <StarRatings
+                                rating={rating}
+                                starDimension="22px"
+                                starSpacing="1px"
+                                starRatedColor="#e8b923"
+                            />
+                        </div>
+                    </div>
+                    <InterestProductCardMini
+                        product={interestAccount}
+                        rating={rating}
+                        realTimeRates={realTimeRates[companyId]}
+                    />
+                </div>
+                <div className="pros-cons-section">
+                    <h2 className="pros-cons-section__title">Pros & Cons</h2>
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-6 pros-cons-section__list">
+                            <h4>Pros</h4>
+                            <ReactMarkdown source={pros} />
+                        </div>
+                        <div className="col-xs-12 col-sm-6 pros-cons-section__list">
+                            <h4>Cons</h4>
+                            <ReactMarkdown source={cons} />
+                        </div>
+                    </div>
+                </div>
+                <div className="body-section">
+                    <ReactMarkdown source={body} />
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="review-page">
             <Head>
